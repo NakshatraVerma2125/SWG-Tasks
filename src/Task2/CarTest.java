7040572f2ccc86c740483155d4dc3005f32769a4
Nakshatra Verma <nakshatraverma2125@gmail.com>
2022-02-26 21:13:06 +0530

    updated CarTest.java

--- src/Task2/CarTest.java
+++ src/Task2/CarTest.java
@@ -1,11 +1,16 @@
 package Task2;
 
 public class CarTest {
-	Car car = new Car(180, 2000000, "Matte Black");
 	
-	Truck truck1 = new Truck(170, 3000000, "Red", 2500);
+	public static void main(String[] args) {
+		Car car = new Car(180, 2000000, "Matte Black");
+		
+		Truck truck1 = new Truck(170, 3000000, "Red", 2500);
 
-	Truck truck2 = new Truck(170, 3000000, "Red", 2500);
-	
-	
+		Truck truck2 = new Truck(170, 3000000, "Red", 1800); // same specs as truck1 but different weight
+		
+		System.out.println("truck1 sale price: " + truck1.getSalePrice());
+
+		System.out.println("truck2 sale price: " + truck2.getSalePrice());
+	}
 }
